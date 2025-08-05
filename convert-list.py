fdc5925b55fb8c252ecc9de18d8f5f6b0bd06c68
Thomas Barrett <trbarrettjr@users.noreply.github.com>
2025-08-04 17:55:42 -0700

    Create convert-list.py
    Initial commit of convert-list.py

---
+++ convert-list.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import sys
+import json
+
+def convert(text):
+    text = text.strip()
+    blocks  = text.split('\n\n')
+    entries = []
+    for block in blocks:
+        entry = {}
+        key = None
+        line = block.split('\n')
+        for item in line:
+            if ":" in item:
+                key, value = item.split(':', 1)
+                if key not in entry:
+                    entry[key.strip()] = value.strip()
+                else:
+                    if not isinstance(entry[key], list):
+                        entry[key] = [entry[key]]
+                    entry[key].append(value.strip())
+            else:
+                value = item.strip()
+                entry[key.strip()] =  entry[key] + ', ' + value.strip()
+
+        entries.append(entry)
+
+    return entries
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        fn = sys.argv[1]
+    else:
+        print(f"Missing Filename!\n{sys.argv[0]} filename")
+        sys.exit(1)
+
+    out_fn = fn.replace('txt', 'json')
+
+    print(f"Processing: {fn}")
+    print()
+
+    with open(fn, 'r') as f:
+        data = convert(f.read())
+
+    with open(out_fn, 'w') as f:
+        json.dump(data, f, indent=2)
+
+    print(f"Process complete\nOut file is {out_fn}")
